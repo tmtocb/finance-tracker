0a8124cca28311a5c5783e5144e12db00c3e901f
tmt-mateusz <tmt.ocb@gmail.com>
2020-11-27 03:23:54 +0100

    Search friends form in my_friends

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,10 @@ class UsersController < ApplicationController
   end
 
   def my_friends
-  	@friends = current_user.friends
+    @friends = current_user.friends
+  end
+
+  def search
+    render json: params[:friend] 
   end
 end
